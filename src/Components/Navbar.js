0ca2a659ab9df5c87c34f7323f1b5b533fdc04b9
Akhil Raghav <rghv064@gmail.com>
2024-11-26 18:52:37 +0530

    handle Concurrent Clients

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
         <img src={logo} alt="logo" className=" w-[6rem] md:w-[8rem]"  />
           {/* Github Link Tab */}
         <div className=" flex flex-row items-center justfy-center space-x-1 md:mr-[0rem] xl:mr-[-6rem] 2xl:mr-[-20rem]  bg-[#f5f6fa] py-[0.3rem] px-[0.5rem] rounded realtive text-[#2f3640] " >
-        <a href="https://github.com/SeemaYadav/expenseTracker" target="_blank">Github</a>
+        <a href="https://github.com/SeemaYadav0/expenseTracker" target="_blank">Github</a>
         <FaGithub/>
         </div>
 
